44142efb8e8509ee9d6cc5553a4506a545a99c3a
ncampolito <nickcampolito@yahoo.com>
2016-01-14 23:47:22 -0800

    Start of Basic UI
    Implementation of a basic health system using player.cs script.
    Essentially it displays 10 "x" characters to indicate health and updates
    when pressing the "x" key to reduce health. This simple implementation
    can be utilized relatively easily for later use when we start using
    enemies and environment to cause damage.

---
+++ camera control/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float playerHealth;
+	public Text healthText;
+	public bool fullHealth;
+	// Use this for initialization
+	void Start () {
+		playerHealth = 10f;
+		healthDisplay(playerHealth);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthDisplay(playerHealth);
+		simulateDamage();
+	}
+	// Displays health
+	void healthDisplay(float health){
+		if (health != 0.0f){
+			healthText.text = "Health: ";
+			for(float i = 0.0f; i < health; i++){
+				healthText.text = healthText.text + "x";
+			}
+			
+		}
+	}
+	/*test if UI updates when taking damage, right now just
+	reduce health by one.*/
+	void simulateDamage(){
+		if(Input.GetKeyUp("x")){
+			playerHealth--;
+			
+		}
+	}
+}
