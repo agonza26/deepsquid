c271dd79dcc98d8c9b3dcc74b0868954e4024a60
agonza26 <agonza26@ucsc.edu>
2016-02-02 17:09:18 -0800

    Added Previous Progress

---
+++ Rising Tide/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemy : MonoBehaviour {
+	public Transform pos1; //until I make an array
+	public Transform pos2; //only three different places to patrol between
+	public Transform pos3;
+
+	public float dg = 5f; //drag
+	public float vel = 5f; //velocity
+
+	public float radius; //how far away from the patrol points should the fist start turning towards the next
+	public float magicNum; //dont remember at the moment
+	public float wiggleVar; // how far away from path should the enemy wander
+	public float swimT; // how frequent the fish "swims", normal
+
+	//not used at the moment
+	/*
+	public float swimPanicT; // how frequent the fish "swims", Panic
+	public float swimAttackT; // how frequent the fish "swims, Attack
+	public float swimHuntT; // how frequent the fish "swims, Hunt
+	*/
+
+
+
+	private int index = 1;
+	private float timeC = 0f;
+	private Rigidbody rigBod;
+	private Transform currentHeading;
+	private Transform currentTarget;
+
+
+
+	//SARA calls
+
+
+
+	void Start () {
+
+		//wish i could use pointers and better copy methods :(
+		currentTarget = pos1; 
+		currentHeading = currentTarget;
+		transform.LookAt(currentHeading);//start off looking at the first point
+
+
+		rigBod = GetComponent<Rigidbody>();//init rigitbody
+		rigBod.velocity = (transform.forward * vel); //assign forward direction velocity 
+		rigBod.drag = dg; //drag slows down enemy to emulate water
+	}
+
+
+
+
+
+	//this model could be used for a steering director possibly
+	void Update () {
+		timeC += Time.deltaTime; //increase timer clock 
+		if (timeC >= swimT) { //if we hit the assigned swim timer, its time to swim
+			swim ();
+			timeC = 0; //reset timer
+		}
+	
+		//if we are near the current target
+		float dist = Vector3.Distance (transform.position, currentTarget.position);
+		if (dist < radius) {
+			changeTarget ();
+		}
+	}
+
+
+
+
+	void swim(bool straight = false, int factor = 1){
+		if (straight) {
+			//Not sure what else to add here at the moment 
+		}else{
+			wiggle (factor); //simple implementation of randomness untill steering ai(backlogged a lot)
+			//same here
+		}
+		rigBod.velocity = (transform.forward * vel);
+
+	}
+
+
+	void wiggle(int factor){
+		Vector3 test = currentHeading.position;
+		test += Vector3.up * Random.Range (-magicNum, magicNum);
+		test += Vector3.down * Random.Range (-magicNum, magicNum);
+		test += Vector3.left * Random.Range (-magicNum, magicNum);
+		test += Vector3.right* Random.Range (-magicNum, magicNum);
+
+
+		//check to see if we are too far
+		float distX = Mathf.Abs(test.x-currentTarget.position.x);
+		float distY = Mathf.Abs(test.y-currentTarget.position.y);
+		float distZ = Mathf.Abs(test.z-currentTarget.position.z);
+		if (distX > wiggleVar||distY > wiggleVar||distZ > wiggleVar) {
+
+			Debug.Log("we are too far off");
+		}
+
+
+		transform.LookAt(test);
+	}
+
+
+
+
+
+
+
+
+
+
+
+	void changeTarget(bool overRide = false, Transform custTarget = null){
+		switch (index) {
+			case 2:
+				index = 3;
+				currentTarget = pos3;
+				break;
+			case 3:
+				index = 1;
+				currentTarget = pos1;
+
+				break;
+			case 1:
+			default:
+				index = 2;
+				currentTarget = pos2;
+				break;
+		}
+		currentHeading = currentTarget; //temporary test
+		transform.LookAt(currentHeading);
+	}
+
+
+
+}
---
+++ Rising Tide/Assets/Scripts/System/CameraController.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform playerCameraTarget;
+	public Transform player;
+	private Vector3 offset = new Vector3 (0f, 0f, -7.5f);
+	private float rcMaxDist;
+	private Vector3 camDistSave;
+	public float minCameraDist = -5f;
+	public float maxCameraDist = -15f;
+	private bool AlphaDrop = false;
+	private Material temp;
+	private bool isTouching = false;
+	private bool CameraChildTouching = false;
+
+	public float positionDampening = 8f; //controls how snappy the camera follows the camera object. a higher number means more snappy. lower number means more flowy
+	public float rotationDampening = 5f; //see above, but applies to rotation
+	private float PDsave;
+
+	private Transform thisTransform;
+
+	public float minX = -360.0f;
+	public float maxX = 360.0f;
+
+	public float minY = -45.0f;
+	public float maxY = 45.0f;
+
+	public float sensX = 100.0f;
+	public float sensY = 100.0f;
+
+	float rotationY = 0.0f;
+	float rotationX = 0.0f;
+
+
+
+	void Start () 
+	{
+		thisTransform = transform; //cache transform default
+		camDistSave = offset;
+		PDsave = positionDampening;
+		//minCameraDistAlphaDropRange = minCameraDist + 1.6f;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		rcMaxDist = -offset.z;
+		Debug.DrawRay(transform.position, transform.forward * rcMaxDist);
+		//Debug.DrawRay(transform.position, -transform.forward * 1f);
+		if(!playerCameraTarget)
+		{
+			return;   //if there is no game object for the camera to follow, return
+		}
+		rotationX += Input.GetAxis ("Mouse X") * sensX * Time.deltaTime;
+		rotationY += Input.GetAxis ("Mouse Y") * sensY * Time.deltaTime;
+		rotationY = Mathf.Clamp (rotationY, minY, maxY);
+
+		//mouse scroll wheel. positive means that mousewheel scroll up. negative means mousewheel scroll down.
+		//controls moving the camera forward and backward facing the object
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0 && offset.z < minCameraDist) 
+		{
+			offset.z += 0.8f;
+			camDistSave.z += 0.8f;
+			rcMaxDist -= 0.8f;
+		} else if (Input.GetAxis ("Mouse ScrollWheel") < 0 && offset.z > maxCameraDist) 
+		{
+			offset.z -= 0.8f;
+			camDistSave.z -= 0.8f;
+			rcMaxDist += 0.8f;
+			AlphaDrop = false;
+		}
+		RaycastHit hit;
+
+
+		if(CameraChildTouching)
+		{
+			// = 1.5f/Time.deltaTime;
+			//float objToPlayr = Vector3.Distance(hit.transform.position, playerCameraTarget.transform.position);
+			offset.z += 0.25f;
+		} else 
+		{
+			//positionDampening = PDsave;
+		}
+
+		/*if(AlphaDrop && player.material.color.a > 0.35f)
+		{
+			player.material.color.a -= 0.2f;
+		}*/
+		if(offset.z > camDistSave.z && !Physics.Raycast(transform.position, transform.forward, rcMaxDist) && !isTouching && ! Physics.Raycast(transform.position, -transform.forward, out hit, 1f))
+		{
+			offset.z -= 0.5f;
+		} 
+
+		if(Physics.Raycast(transform.position, transform.forward, out hit, rcMaxDist))
+		{
+			float objToPlayr = Vector3.Distance(hit.transform.position, playerCameraTarget.transform.position);
+			float camToPlayr = Vector3.Distance(transform.position, playerCameraTarget.transform.position);
+			float pushCfrwrd = camToPlayr - objToPlayr;
+			offset.z = -1f * objToPlayr + 1.125f;
+			/*if(pushCfrwrd > 0)
+			{
+				offset.z += pushCfrwrd;
+			} else 
+			{
+				offset.z -= pushCfrwrd;
+			}*/
+			positionDampening = Mathf.Abs(pushCfrwrd/Time.deltaTime);
+
+			Debug.Log("pushCfrwrd " + pushCfrwrd);
+		}
+		Vector3 wantedPosition = playerCameraTarget.position + (playerCameraTarget.rotation * offset);
+		Vector3 currentPosition = Vector3.Lerp(thisTransform.position, wantedPosition, positionDampening * Time.deltaTime);
+
+		thisTransform.position = currentPosition;
+		//Quaternion wantedRotation = Quaternion.LookRotation (playerCameraTarget.position - thisTransform.position, playerCameraTarget.up);
+
+		//thisTransform.rotation = wantedRotation;
+
+		transform.LookAt(playerCameraTarget);
+		positionDampening = PDsave;
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		isTouching = true;
+		Debug.Log("Camera collided with wall");
+	}
+
+	private void OnTriggerExit (Collider collider)
+	{
+		isTouching = false;
+	}
+
+	public void InnerCamT()
+	{
+		CameraChildTouching = true;
+		//positionDampening = 1f/Time.deltaTime;
+		Debug.Log("CameraChild collision triggered");
+	}
+
+	public void InnerCamNT()
+	{
+		CameraChildTouching = false;
+		//positionDampening = PDsave;
+		Debug.Log("CameraChild left the wall");
+	}
+
+	public static float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp(angle, min, max);
+	}
+}
