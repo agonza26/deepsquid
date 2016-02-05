77f8cf64eb31baf25a3df5578dac00f9c927668d
Gina Nagel <gnagel@ucsc.edu>
2016-02-04 19:50:00 -0800

    adding pickup scripts
    adding Pickupable.cs and PickUpObject.cs scripts to Scripts Folder
    (Scripts->Player)

---
+++ Rising Tide/Assets/Scripts/Player/PickupObject.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+press 'e' to grab an object, press 'e' again to release it
+    if object's size is greater than player's, player will anchor to it
+    else object will anchor to player
+press 'q' to throw a held object (you can't throw objects bigger than you)
+
+attach Pickupable script to any object that player should be able to pick up
+attach PickupObject script to player
+put the player model object on layer "Ignore Raycast" or it will get in the way
+*/
+
+public class PickupObject : MonoBehaviour
+{
+
+    GameObject mainCamera;
+    bool carrying;
+    GameObject carriedObject;
+    float objectSize;
+    bool parented = false;
+    bool canThrow;
+
+    public GameObject player;
+    public float playerSize = 2.0f;
+    public float distance = 1.5f; //offset between carried object and player
+    public float smooth = 8f; //smooth carrying movement
+    public float throwForce = 700f;
+    private Quaternion playerZRot;
+    //public float rayDistance; //how far away an object can be picked up from
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (carrying)
+        {
+            carry(carriedObject);
+            checkDrop();
+        }
+        else
+        {
+            pickup();
+        }
+
+    }
+
+    void carry(GameObject o)
+    {
+        o.layer = 2; //put grabbed object on IgnoreRaycast layer (to avoid camera freakouts
+
+        if (playerSize > objectSize) //object is smaller than player
+        {
+            canThrow = true;
+            o.GetComponent<Rigidbody>().useGravity = false;
+            //put the object below player, move sorta smoothly with Lerp
+            float d = (o.GetComponent<Collider>().bounds.size.z)/2f;
+            playerZRot = player.transform.rotation;
+            o.transform.position = Vector3.Lerp(o.transform.position, player.transform.position + (-player.transform.up * d) * distance, Time.deltaTime * smooth);
+            o.transform.rotation = playerZRot; //stop picked up object from rotating independently
+        }
+
+        else //object is larger than player
+        {
+            canThrow = false; // get rid of this for super squid strength
+            if (!parented)
+            {
+                //position player alongside grabbed object
+                Vector3 objSize = (o.GetComponent<Collider>().bounds.size) / 2f;
+                player.transform.position = Vector3.Lerp(player.transform.position, o.transform.position - objSize, Time.deltaTime * smooth);
+
+                //make player child of grabbed object for movement and rotation
+                player.transform.parent = o.transform.GetChild(0); //emptyObject, child of grabbed one, to avoid distortion of player model
+                player.GetComponent<Rigidbody>().isKinematic = true; //without this it won't move with the parent
+                parented = true;
+                //o.layer = 2; //move grabbed ogject to IgnoreRaycast layer (to avoid camera freakouts)
+
+            }
+            Debug.Log("anchor to big object");
+        }
+    }
+
+    void pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //shoot ray from center of screen
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y)); //shoot ray from middle of screen 
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction);
+            if (Physics.Raycast(ray, out hit))
+            //if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                Debug.Log(hit.transform.gameObject);
+                //if it hits something valid, pick it up
+                Pickupable p = hit.collider.GetComponent<Pickupable>(); 
+                if (p != null)
+                {
+                    Debug.Log("that can be picked up");
+                    carrying = true;
+                    carriedObject = p.gameObject;
+                    //p.gameObject.GetComponent<Rigidbody>().isKinematic = true; //not used //so that we can move the object around w/o it being affected by gravity, etc
+                    //gameObject.GetComponent<Rigidbody>().useGravity = false; //moved this line to carry function
+                    objectSize = p.size;
+                }
+            }
+        }
+    }
+
+    void checkDrop()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            dropObject();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (canThrow)
+            {
+                throwObject();
+            }
+        }
+    }
+
+    void dropObject()
+    {
+        carrying = false;
+        carriedObject.layer = 0; //return carried object to default layer
+        if (parented)
+        {
+            player.transform.parent = null;
+            parented = false;
+            player.GetComponent<Rigidbody>().isKinematic = false;            
+            Debug.Log("releasing big object");
+        }
+        //carriedObject.GetComponent<Rigidbody>().useGravity = true; //disabling for now
+        carriedObject = null;
+    }
+
+    void throwObject()
+    {
+        //carrying = false;
+       
+        //carriedObject.GetComponent<Rigidbody>().useGravity = true;
+        carriedObject.GetComponent<Rigidbody>().AddForce(transform.forward * throwForce);
+        dropObject();
+        //carriedObject = null;
+    }
+}
