f0d7eb51a7a07ca160e95c1666f5ca9e1c1293fb
jsngai <justin.ngai@gmail.com>
2016-02-04 17:41:31 -0800

    3a finished, 3b finished

---
+++ Rising Tide/Assets/Scripts/Player/Player_stats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_stats : MonoBehaviour {
+	
+	public float PlayerHealthMax = 10f;
+	public float PlayerOverhealthMax;
+	public float PlayerCurrHealth;
+	public bool PlayerDmged;
+
+	// Use this for initialization
+	void Start () {
+		PlayerCurrHealth = PlayerHealthMax;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void playerDamage(float val)
+	{
+		PlayerDmged = true;
+		PlayerCurrHealth -= val;
+	}
+}
--- Rising Tide/Assets/Scripts/System/Player.cs
+++ Rising Tide/Assets/Scripts/System/Player.cs
@@ -4,48 +4,69 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	
-	public float playerHealthTotal;
-	public float playerHealthCurr;
+	private GameObject player;
+	private float playerHealthTotal;
+	private float playerHealthCurr;
 	public Slider healthBar;
 	public Color flashColor = new Color (1f,0f,0f,0.1f);
 	public Image hideBar;	
 	public Image DamageIndicator;
 	public float flashSpeed = 5f;
-	bool damaged;
+	//bool damaged;
 	bool isAlive;
 	
 
 	void Start () {
 		//Set initial health of player;
-		playerHealthTotal = 10f;
-		playerHealthCurr = playerHealthTotal;	
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerHealthTotal = player.GetComponent<Player_stats>().PlayerHealthMax;
+	
 		isAlive = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if (damaged){
+		
+		if(Input.GetKeyUp("x"))
+		{
+			player.GetComponent<Player_stats>().playerDamage(1f);
+			//damage = true;
+		}
+		
+		playerHealthCurr = player.GetComponent<Player_stats>().PlayerCurrHealth;
+		healthBar.value = playerHealthCurr;
+		
+		if(playerHealthCurr <= 0f  && isAlive)
+		{
+			hideBar.color = Color.Lerp(Color.clear, Color.black, playerHealthCurr/playerHealthTotal);
+		}
+		
+		if (player.GetComponent<Player_stats>().PlayerDmged)
+		{
 			DamageIndicator.color = flashColor;
 		}
-		else{
+		else
+		{
 			DamageIndicator.color = Color.Lerp(DamageIndicator.color, Color.clear, flashSpeed * Time.deltaTime);
 		}
-		damaged = false;
-		if(Input.GetKeyUp("x")){
-			simulateDamage(1f);
-		}
+		
+		player.GetComponent<Player_stats>().PlayerDmged = false;
+		
+		
+		
 
 	}
 
 	/*test if UI updates when taking damage, right now just
 	reduce health by one.*/
-	public void simulateDamage(float val){
+	/*public void simulateDamage(float val){
 		damaged = true;
+		playerHealthCurr = player.GetComponent<Player_stats>().playerHealthCurr
 		playerHealthCurr -= val;
 		healthBar.value = playerHealthCurr;
 		if(playerHealthCurr <= 0f  && isAlive){
 			hideBar.color = Color.Lerp(Color.clear, Color.black, playerHealthCurr/playerHealthTotal);
 		}
-	}
+	}*/
 }
