ebf447ac4bdef2f6c9c1452616e767f9aeb1f64a
ncampolito <nickcampolito@yahoo.com>
2016-02-07 01:18:35 -0800

    Introduction of Abilities
    This iteration just includes the ink particle system. Right now it has
    an internal CD of 5 seconds, and the 'ink' particles will collide with
    terrain.  There are some weird bugs, but once integrated, should be able
    to interfere with AI

---
+++ Rising Tide/Assets/Scripts/System/Abilities.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Abilities : MonoBehaviour {
+
+	public ParticleSystem Ink;
+	public Transform playerPos;
+	//if its true, that means its on CD and cannot be used, if false then usable.
+
+	public float inkCDTimer = 0;
+	public float inkCD = 5;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space") && Time.time > inkCDTimer) {
+			inkCDTimer = Time.time + inkCD;
+			newInk ();
+
+		} else if (Input.GetKeyDown ("space") && Time.time < inkCDTimer) {
+			Debug.Log ("Ink is on CD");
+		} else {
+		}
+			
+	}
+
+	void newInk(){
+		
+		Ink.transform.position = playerPos.transform.position;
+		//Ink.sizeOverLifetime.enabled = true;
+		ParticleSystem.SizeOverLifetimeModule sz = Ink.sizeOverLifetime;
+		sz.enabled = true;
+		Ink.Play ();
+	}
+
+
+}
