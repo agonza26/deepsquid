3299bb79e2056539d9f09a6979517e3f511542e7
alexv510 <aovallad@ucsc.edu>
2016-02-07 17:19:17 -0800

    Add Walls and Player Opacity
    Codes in there, waiting on camera for it to be fully implemented. Walls
    have some holes to cover

--- Rising Tide/Assets/Scripts/Player/Player_stats.cs
+++ Rising Tide/Assets/Scripts/Player/Player_stats.cs
@@ -7,10 +7,15 @@ public class Player_stats : MonoBehaviour {
 	public float PlayerOverhealthMax;
 	public float PlayerCurrHealth;
 	public bool PlayerDmged;
-
+	private Color lowAlph;
+	private Color normalAlph;
+	
 	// Use this for initialization
 	void Start () {
 		PlayerCurrHealth = PlayerHealthMax;
+		normalAlph = GetComponent<Renderer>().material.color;
+		lowAlph = GetComponent<Renderer>().material.color;
+		lowAlph.a = 0.65f;
 	}
 	
 	// Update is called once per frame
@@ -22,5 +27,22 @@ public class Player_stats : MonoBehaviour {
 	{
 		PlayerDmged = true;
 		PlayerCurrHealth -= val;
+	}
+	
+	public void changePlayerAlphaDown()
+	{
+		//GetComponent<Renderer>().material.SetColor("_Color", lowAlph);
+		GetComponent<Renderer>().material.color = Color.Lerp(GetComponent<Renderer>().material.color, lowAlph, 5f * Time.deltaTime);		
+		/*Debug.Log("Desired Color: " + lowAlph);
+		Debug.Log("Current Color: " + GetComponent<Renderer>().material.color);*/
+	}
+	
+	public void changePlayerAlphaUp()
+	{
+		if(GetComponent<Renderer>().material.color.a > normalAlph.a)
+		{
+			return;
+		}
+		GetComponent<Renderer>().material.color = Color.Lerp(GetComponent<Renderer>().material.color, normalAlph, 5f * Time.deltaTime); //Color.Lerp(lowAlph, GetComponent<Renderer>().material.color, 5f * Time.deltaTime);
 	}
 }
