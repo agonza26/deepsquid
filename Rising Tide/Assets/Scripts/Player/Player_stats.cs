e32a673768d0e623a686a9ab9d04359038d5f9a9
alexv510 <aovallad@ucsc.edu>
2016-02-09 14:08:27 -0800

    Death
    When player runs out of health, level restarts
    Made a death screen, still needs to be implemented with player death.

--- Rising Tide/Assets/Scripts/Player/Player_stats.cs
+++ Rising Tide/Assets/Scripts/Player/Player_stats.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
 
 public class Player_stats : MonoBehaviour {
 	
 	public float PlayerHealthMax = 10f;
 	public float PlayerOverhealthMax;
 	public float PlayerCurrHealth;
+    public float restartDelay = 5f;
 	public bool PlayerDmged;
 	private Color lowAlph;
 	private Color normalAlph;
-	
-	// Use this for initialization
-	void Start () {
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
 		PlayerCurrHealth = PlayerHealthMax;
 		normalAlph = GetComponent<Renderer>().material.color;
 		lowAlph = GetComponent<Renderer>().material.color;
@@ -22,14 +33,32 @@ public class Player_stats : MonoBehaviour {
 	void Update () {
 	
 	}
-	
-	public void playerDamage(float val)
+
+
+    public void playerDamage(float val)
 	{
 		PlayerDmged = true;
 		PlayerCurrHealth -= val;
+
+        if (PlayerCurrHealth <= 0)
+        {
+            /*if (gameObject.CompareTag("Player"))
+            {
+                Destroy(gameObject);
+                Debug.Log("player dead");
+                SceneManager.LoadScene("Backup");
+            }*/
+            anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+
+            if(restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene("Backup");
+            }
+        }
 	}
-	
-	public void changePlayerAlphaDown()
+
+    public void changePlayerAlphaDown()
 	{
 		//GetComponent<Renderer>().material.SetColor("_Color", lowAlph);
 		GetComponent<Renderer>().material.color = Color.Lerp(GetComponent<Renderer>().material.color, lowAlph, 5f * Time.deltaTime);		
