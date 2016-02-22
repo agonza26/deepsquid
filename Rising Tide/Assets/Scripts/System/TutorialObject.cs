3a3a910e7dd17c902c04aaf65f2bf8cb91759530
ncampolito <nickcampolito@yahoo.com>
2016-02-21 16:29:55 -0800

    Implementation of Tutorial Objects
    It works.

---
+++ Rising Tide/Assets/Scripts/System/TutorialObject.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialObject : MonoBehaviour {
+
+public bool debugStatements = true;
+          // Last place this enemy spotted the player.
+
+		public SphereCollider col;                     // Reference to the sphere collider trigger component.
+		public GameObject tutorialText;
+		public GameObject player;                      // Reference to the player.
+              // Where the player was sighted last frame.
+		
+		//private float timer = 0f;
+		public float timerLimit = 5f;
+
+		void Start(){
+			tutorialText.SetActive(false);
+		}
+		void Awake ()
+		{
+			tutorialText.SetActive(false);
+			//col = GetComponent<SphereCollider>();
+			player = GameObject.FindGameObjectWithTag("Player");
+
+		}
+
+
+		void Update ()
+		{
+
+
+		}
+
+
+		
+
+
+
+
+
+		void OnTriggerStay (Collider other)
+		{
+			// If the player has entered the trigger sphere...
+			if(other.gameObject == player)
+			{
+				Debug.Log("We hella now");
+				tutorialText.SetActive(true);
+				
+			}
+
+
+		}
+
+
+
+
+
+		void OnTriggerExit (Collider other)
+		{
+			// If the player leaves the trigger zone...
+			if (other.gameObject == player) {
+				tutorialText.SetActive(false);
+				
+			}
+		}
+}
