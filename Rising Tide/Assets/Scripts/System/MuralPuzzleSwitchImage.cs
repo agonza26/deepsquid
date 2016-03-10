e0a7ec30ac2896646ac401f34ec90d70a1489ffc
jsngai <justin.ngai@gmail.com>
2016-03-09 17:34:24 -0800

    computer's driver just crashed like shit
    making a safety commit

---
+++ Rising Tide/Assets/Scripts/System/MuralPuzzleSwitchImage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuralPuzzleSwitchImage : MonoBehaviour {
+
+	public Texture[] frames;
+	public int frameIndex = 0;
+	private Projector projector;
+	private bool inked = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		projector = GetComponent<Projector> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		projector.material.SetTexture ("_ShadowTex", frames [frameIndex]);
+	}
+
+	public void ChangeToInked(int newInd)
+	{
+		frameIndex = newInd;
+	}
+
+}
---
+++ Rising Tide/Assets/Scripts/System/inkGateObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inkGateObject : MonoBehaviour {
+
+    bool isActive;
+
+    public Transform target;
+    public Transform defaultPos;
+    public float speed;
+
+    public GameObject inkObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        //inkObject = GameObject.Find("InkMural");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(inkObject.GetComponent<inkObjectPuzzle>().activated);
+        isActive = inkObject.GetComponent<inkObjectPuzzle>().activated;
+
+        if (isActive)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+        else 
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, defaultPos.position, step);
+        }
+    }
+}
