322d3cd6c73bb4df76a307549a880210c59d9685
alexv510 <aovallad@ucsc.edu>
2016-03-10 18:09:26 -0800

    scenery change

---
+++ Rising Tide/Assets/LabSwitch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabSwitch : MonoBehaviour {
+
+	public GameObject gate;
+	public Transform origPos;
+	public Vector3 movedPos = new Vector3(0,0,0);
+	public Transform goBack;
+	public float dist = 30f;
+	bool boxCollided = false; 
+
+	// Use this for initialization
+	void Start () {
+		origPos = gate.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Position: " + gate.transform.localPosition);
+	}
+
+	void OnTriggerStay(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("box")) {
+			if (movedPos.x < dist) {
+				gate.transform.localPosition += new Vector3 (0.1f, 0, 0);
+				movedPos += new Vector3 (0.1f, 0, 0);
+			}//= Vector3.Lerp (origPos, movedPos, 0.1f);
+		}
+	}
+}
