07292799029e99c3c025dd9fcbeb46a242b731c5
alexv510 <aovallad@ucsc.edu>
2016-05-15 13:23:07 -0700

    alex stashin shit
    hide the weed

---
+++ Rising Tide/Assets/Data/Scripts/ScrollTexture.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ScrollTexture : MonoBehaviour {
+
+	public Vector2 Scroll = new Vector2 (0.05f , 0.05f);
+	Vector2 Offset = new Vector2 (0f, 0f);
+
+	void Update () {
+		Offset +=  Scroll * Time.deltaTime;
+		GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", Offset);
+	}
+}
+
