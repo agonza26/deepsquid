e65a63438e39b2058718cef18f04f3b9114c83bc
agonza26 <agonza26@ucsc.edu>
2016-06-01 13:37:04 -0700

    gate and fridge in. pathways made
    fridge added to KF area
    gate and switch added to KG
    pathways to different parts of the cave made

---
+++ Rising Tide/Assets/Data/Scripts/FridgeModelSwitch.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FridgeModelSwitch : MonoBehaviour {
+
+	public GameObject other;
+	public bool isFirst = false;
+	public GameObject milk;
+
+	// Use this for initialization
+	void Start () {
+		milk = GameObject.Find("milkBox");
+		other = GameObject.Find("BrokenFridge");
+		if (!isFirst) {
+			gameObject.SetActive (false);
+		} else {
+			other.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision o){
+		if (o.gameObject.GetComponent<Pickupable> () && other) {
+			if (o.gameObject.GetComponent<Pickupable> ().isThrown ())
+					changeModels ();
+			}
+		
+		
+	}
+
+    //allow breakage even if thrown while already in collider?
+    void OnCollisionStay(Collision o)
+    {
+        if (o.gameObject.GetComponent<Pickupable>() && other)
+        {
+            if (o.gameObject.GetComponent<Pickupable>().isThrown())
+            {
+                print ("here in is Thrown");
+				o.gameObject.GetComponent<Pickupable> ().changeThrown ();
+                changeModels();
+            }
+
+        }
+    }
+
+
+    private void changeModels(){
+		other.SetActive (true);
+		gameObject.SetActive (false);
+		milk.GetComponent<Rigidbody>().isKinematic = false;
+	}
+}
